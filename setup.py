c4b24914ae8c618dfd6e2c24a44676f2aaf88a26
K.A. Oman <koman@astro.rug.nl>
2017-10-17 14:48:26 +0200

    Barebones module setup.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='simobj',
+    version='0.1',
+    description='Code abstraction of objects (galaxies) in simulations.',
+    url='',
+    author='Kyle Oman',
+    author_email='koman@astro.rug.nl',
+    license='',
+    packages=['simobj'],
+    install_requires=['numpy', 'astropy', 'simfiles'],
+    include_package_data=True,
+    zip_safe=False
+)
